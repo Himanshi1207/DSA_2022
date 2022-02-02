51ccece0f58b14ac1af9a2bbc152230dcf9bd856
Himanshi <kanojiahimanshi1221@gmail.com>
2022-02-02 10:04:12 +0530

    first ques added

---
+++ 1.TrailingZeros.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int trailingZeros(int n)
+{
+    int res = 0;
+    for (int i = 5; i <= n; i *= 5)
+        res = res + n / i;
+    return res;
+}
+int main()
+{
+    int n;
+    cin >> n;
+    cout<<"The trailing zeroes in the number is "<<trailingZeros(n);
+    return 0;
+}
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # DSA_10JAN2022
+---
+>### Ques 1: Trailing Zeros 
+>> Time Complexity: log(n)
+<br />
+>> Auxiliary Space: Θ(1)
+---
