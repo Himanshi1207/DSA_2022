849fe2aabe86fc087f2adf0f3ec71d60b5019a13
Himanshi <kanojiahimanshi1221@gmail.com>
2022-05-21 19:42:37 +0530

    did questions related to graphs

---
+++ 67.CycleDetectionUndirectedGraph.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+bool iscycle(int src, vector<vector<int>> &adj, vector<bool> &visited, int parent)
+{
+    visited[src] = true;
+    for (auto i : adj[src])
+    {
+        if (i != parent)
+        {
+            if (visited[i])
+                return true;
+            if (!visited[i] and iscycle(i, adj, visited, src))
+            {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    vector<vector<int>> adj(n);
+    for (int i = 0; i < m; i++)
+    {
+        int x, y;
+        cin >> x >> y;
+        adj[x].push_back(y);
+        adj[y].push_back(x);
+    }
+    bool cycle = false;
+    vector<bool> visited(n, false);
+    for (int i = 0; i < n; i++)
+    {
+        if (!visited[i] and iscycle(i, adj, visited, -1))
+        {
+            cycle = true;
+        }
+    }
+    if (cycle)
+    {
+        cout << "cycle is present" << endl;
+    }
+    else
+    {
+        cout << "cycle is NOT present" << endl;
+    }
+    return 0;
+}
